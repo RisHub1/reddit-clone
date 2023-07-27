2a497a4d80505a9d7bb82d4e3a5a1898a0d73db1
Rishabh Ranjan <rishabhmranjan@gmail.com>
2023-07-27 11:39:03 -0400

    Add login modal button

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,6 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import AuthModal from "../../Modal/AuthModal/AuthModal";
 
 type RightContentProps = {
   //user: any;
@@ -9,7 +10,7 @@ type RightContentProps = {
 const RightContent: React.FC<RightContentProps> = () => {
   return (
     <>
-      {/* <AuthModel/> */}
+      <AuthModal />
       <Flex justify="center" align="center">
         <AuthButtons />
       </Flex>
